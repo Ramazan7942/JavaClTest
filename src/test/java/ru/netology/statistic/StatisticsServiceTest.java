382f8b1d66bf8b8186cd0550cb9c01f6f68db340
Ramazan Alipov <ram.alipov@yandex.ru>
2023-01-21 02:35:17 +0300

    Change

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,9 +18,10 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
     @Test
     void findMaximumInMeddle() {
-        StatisticsService service =new StatisticsService();
+        StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {1, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
